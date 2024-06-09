8947e0f3bde821a49fa36005379a7f0eaa47d680
Abhay218 <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2024-06-09 15:44:41 +0530

    Count Zero by using Recursion

---
+++ Recursion-2/CountZero.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountZero {
+    public static int countZero(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 10) {
+            return 0;
+        } else if (n % 10 == 0) {
+
+            return 1 + countZero(n / 10);
+        }
+        return countZero(n / 10);
+
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        System.out.println(countZero(n));
+    }
+}
